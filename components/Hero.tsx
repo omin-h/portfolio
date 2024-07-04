dfde4ec13c154e82bc085503321f036483e9eb43
Omin Hemapriya <it21801372@my.sliit.lk>
2024-07-04 16:31:30 +0530

    Main page created

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Spotlight } from './ui/spotlight'
 
 const Hero = () => {
   return (
-    <div>
+    <div className='pb-20 pt-36'>
+      <div>
+
+        <Spotlight className='-top-40 -'/>
+
+      </div>
       
     </div>
   )
