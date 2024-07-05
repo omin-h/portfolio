10b0dd29caaf097d99abc7e53d29d788759da712
Omin Hemapriya <it21801372@my.sliit.lk>
2024-07-05 16:55:20 +0530

    Heading text updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
          bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
         
-       </div>
+      </div>
 
        <div className='flex justify-center relative my-20 z-10'>
           <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw]
@@ -27,13 +27,19 @@ const Hero = () => {
             <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic Web Magic with Next.js
           </p>
+          
 
           <TextGenerateEffect 
             className='text-center text-[40px] md:text-5xl lg:text-6xl'
             words="Hello world!, I'm Omin"
             />
 
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi! I&apos;m Omin, a fullstack Developer based in Sri Lanka.
+          </p>
+
           </div>
+          
         </div>
 
 
--- components/ui/text-generate-effect.tsx
+++ components/ui/text-generate-effect.tsx
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import { motion, stagger, useAnimate } from "framer-motion";
 import { cn } from "@/utils/cn";
 
+
 export const TextGenerateEffect = ({
   words,
   className,
@@ -13,6 +14,7 @@ export const TextGenerateEffect = ({
   const [scope, animate] = useAnimate();
   let wordsArray = words.split(" ");
   useEffect(() => {
+    console.log(wordsArray);
     animate(
       "span",
       {
@@ -32,7 +34,9 @@ export const TextGenerateEffect = ({
           return (
             <motion.span
               key={word + idx}
-              className="dark:text-white text-black opacity-0"
+              // change here if idx is greater than 3, change the text color to #CBACF9
+              className={` ${idx > 3 ? "text-purple" : "dark:text-white text-black"
+                } opacity-0`}
             >
               {word}{" "}
             </motion.span>
@@ -44,8 +48,10 @@ export const TextGenerateEffect = ({
 
   return (
     <div className={cn("font-bold", className)}>
-      <div className="mt-4">
-        <div className=" dark:text-white text-black text-2xl leading-snug tracking-wide">
+      {/* mt-4 to my-4 */}
+      <div className="my-4">
+        {/* remove  text-2xl from the original */}
+        <div className=" dark:text-white text-black leading-snug tracking-wide">
           {renderWords()}
         </div>
       </div>
