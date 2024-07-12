8ab2aeda0df32cf5370798535e034f845ead6a49
Omin Hemapriya <it21801372@my.sliit.lk>
2024-07-12 21:32:11 +0530

    About section updated

--- components/about.tsx
+++ components/about.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import React from "react";
 import { WobbleCard } from "./ui/wobble-card";
 
+
 export function WobbleCardDemo() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 max-w-7xl mx-auto w-full">
@@ -29,11 +30,16 @@ export function WobbleCardDemo() {
       </WobbleCard>
       <WobbleCard containerClassName="col-span-1 min-h-[300px]">
         <h2 className="max-w-80  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
-          No shirt, no shoes, no weapons.
+        I'm flexible with communication across different time zones.
         </h2>
-        <p className="mt-4 max-w-[26rem] text-left  text-base/6 text-neutral-200">
-          If someone yells “stop!”, goes limp, or taps out, the fight is over.
-        </p>
+        <Image
+          src="/gl.jpeg"
+          width={200}
+          height={200}
+          alt="linear demo image"
+          className="absolute -right-10 md:-right-[40%] lg:-right-[20%] -bottom-10 object-contain rounded-2xl"
+        />
+        
       </WobbleCard>
       <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-blue-900 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
         <div className="max-w-sm">
