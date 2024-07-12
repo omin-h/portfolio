b433d9cdfbd519ec42dba75a463417a12bccb726
Omin Hemapriya <it21801372@my.sliit.lk>
2024-07-12 17:32:47 +0530

    project cards finished

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -6,7 +6,7 @@ const RecentProjects = () => {
   return (
     <div className="py-20">
       <h1 className="heading">
-        My{" "}
+        My recent{" "}
         <span className="text-purple">projects</span>
       </h1>
       <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
@@ -19,17 +19,11 @@ const RecentProjects = () => {
               title="GitHub"
               href={item.link}
             >
-              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
-                <div
-                  className="relative w-full h-full overflow-hidden lg:rounded-3xl"
-                  style={{ backgroundColor: "#13162D" }}
-                >
-                  <img src="/bg.png" alt="bgimg" />
-                </div>
+              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[10vh] lg:h-[27vh] mb-10">
                 <img
                   src={item.img}
                   alt="cover"
-                  className="z-10 absolute bottom-0"
+                  className="z-10 absolute bottom-0 rounded-xl"
                 />
               </div>
 
