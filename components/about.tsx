729ea0eb2f4d8a23bfc9e01541e47bb710c501fd
Omin Hemapriya <it21801372@my.sliit.lk>
2024-07-18 16:43:42 +0530

    UI updated

--- components/about.tsx
+++ components/about.tsx
@@ -48,7 +48,7 @@ export function WobbleCardDemo() {
         </h2>
       </WobbleCard>
 
-      <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-pink-800 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
+      <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-pink-800 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]  flex items-center justify-center">
         <div className="max-w-sm">
           <h2 className="max-w-sm md:max-w-lg  text-left text-balance text-base md:text-xl lg:text-3xl font-semibold tracking-[-0.015em] text-white">
             Skills/ Tools
