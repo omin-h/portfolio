657c1c4e4e16923fffd010ea5f41da04bfb1e9d4
Omin Hemapriya <it21801372@my.sliit.lk>
2024-07-22 13:22:41 +0530

    Footer completed

--- components/ui/Footer.jsx
+++ components/ui/Footer.jsx
@@ -1,26 +1,30 @@
 "use client";
 
-import Link from 'next/link';
-import { socialLinks } from "../../constants/index";
+import { FaSquareGithub } from "react-icons/fa6";
+import { IoLogoLinkedin } from "react-icons/io5";
+import { FaSquareXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <footer className='footer font-poppins'>
+    <footer className='footer font-poppins  mb-4'>
       <hr className='border-slate-200' />
-      <div className='footer-container'>
-        <p>
+      <div className='footer-container flex justify-between items-center pb-4'>
+        <p className="">
           © 2024 <strong>Omin Hemapriya</strong>. All rights reserved.
         </p>
-        <div className='flex gap-3 justify-center items-center'>
-          {socialLinks.map((link) => (
-            <a key={link.name} href={link.link} target='_blank' rel='noopener noreferrer'>
-              <img
-                src={link.iconUrl}
-                alt={link.name}
-                className='w-6 h-6 object-contain'
-              />
-            </a>
-          ))}
+
+        <div className="flex gap-5 pt-2 mb-4">
+          <a href='https://github.com/omin-h' aria-label="GitHub">
+            <FaSquareGithub size={30} />
+          </a>
+
+          <a href='https://linkedin.com/in/omin-hemapriya' aria-label="LinkedIn">
+            <IoLogoLinkedin size={30} />
+          </a>
+
+          <a href='https://x.com/ominduln' aria-label="X">
+            <FaSquareXTwitter size={30} />
+          </a>
         </div>
       </div>
     </footer>
