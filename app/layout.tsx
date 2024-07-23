a2615851b92597ed7ff7955f60b43c589673616f
Omin Hemapriya <it21801372@my.sliit.lk>
2024-07-23 18:13:20 +0530

    Added the icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Omin's portfolio",
   description: "Modern portfolio for Omin",
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
